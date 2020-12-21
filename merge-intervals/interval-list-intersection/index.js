28953c596b794284edce8ecc833ea196ee2319ca
Lean <theleanmiguel@gmail.com>
2020-12-20 17:48:19 -0700

    initial commit

---
+++ merge-intervals/interval-list-intersection/index.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/interval-list-intersections/
+
+/**
+ * @param {number[][]} A
+ * @param {number[][]} B
+ * @return {number[][]}
+ */
+var intervalIntersection = function(A, B) {
+    return A+B;
+};
+
+
+module.exports = intervalIntersection;
