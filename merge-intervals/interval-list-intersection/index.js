6c9225b3d71d0fc2e278069556b48f959b8ca4a5
Lean <theleanmiguel@gmail.com>
2020-12-22 20:57:36 -0700

    finish interval intersection

--- merge-intervals/interval-list-intersection/index.js
+++ merge-intervals/interval-list-intersection/index.js
@@ -5,9 +5,35 @@
  * @param {number[][]} B
  * @return {number[][]}
  */
-var intervalIntersection = function(A, B) {
-    return A+B;
-};
 
+const isOverlapping = ([x1, x2], [y1, y2]) =>
+  Math.max(x1, y1) <= Math.min(x2, y2);
+const getIntersection = ([x1, x2], [y1, y2]) => [
+  Math.max(x1, y1),
+  Math.min(x2, y2),
+];
+
+var intervalIntersection = function (A, B) {
+  const intersections = [];
+
+  // if there's an empty list, there's no intersection
+  if (A.length === 0 || B.length === 0) return intersections;
+  let l = 0;
+  let r = 0;
+
+  while (l < A.length && r < B.length) {
+    if (isOverlapping(A[l], B[r])) {
+      intersections.push(getIntersection(A[l], B[r]));
+    }
+
+    if (A[l][1] <= B[r][1]) {
+      l += 1;
+    } else {
+      r += 1;
+    }
+  }
+
+  return intersections;
+};
 
 module.exports = intervalIntersection;
--- merge-intervals/interval-list-intersection/index.test.js
+++ merge-intervals/interval-list-intersection/index.test.js
@@ -1,5 +1,149 @@
-const intervalIntersection = require('./index');
+const intervalIntersection = require("./index");
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(intervalIntersection(1, 2)).toBe(3);
+test("test case 1 ", () => {
+  expect(
+    intervalIntersection(
+      [
+        [0, 2],
+        [5, 10],
+        [13, 23],
+        [24, 25],
+      ],
+      [
+        [1, 5],
+        [8, 12],
+        [15, 24],
+        [25, 26],
+      ]
+    )
+  ).toMatchObject([
+    [1, 2],
+    [5, 5],
+    [8, 10],
+    [15, 23],
+    [24, 24],
+    [25, 25],
+  ]);
+});
+
+test("test case 2 ", () => {
+  expect(
+    intervalIntersection(
+      [
+        [1, 3],
+        [5, 7],
+        [9, 12],
+      ],
+      [[5, 10]]
+    )
+  ).toMatchObject([
+    [5, 7],
+    [9, 10],
+  ]);
+});
+
+test("test case 3 ", () => {
+  expect(
+    intervalIntersection(
+      [[8, 15]],
+      [
+        [2, 6],
+        [8, 10],
+        [12, 20],
+      ]
+    )
+  ).toMatchObject([
+    [8, 10],
+    [12, 15],
+  ]);
+});
+
+test("test case 4 ", () => {
+  expect(
+    intervalIntersection(
+      [[17, 20]],
+      [
+        [2, 3],
+        [6, 8],
+        [12, 14],
+        [19, 20],
+      ]
+    )
+  ).toMatchObject([[19, 20]]);
+});
+
+test("test case 5 ", () => {
+  expect(
+    intervalIntersection(
+      [
+        [1, 3],
+        [5, 6],
+        [7, 9],
+      ],
+      [
+        [2, 3],
+        [5, 7],
+      ]
+    )
+  ).toMatchObject([
+    [2, 3],
+    [5, 6],
+    [7, 7],
+  ]);
+});
+
+test("test case 6 ", () => {
+  expect(
+    intervalIntersection(
+      [
+        [1, 3],
+        [7, 8],
+        [14, 16],
+      ],
+      [
+        [0, 2],
+        [5, 6],
+        [7, 12],
+        [16, 20],
+      ]
+    )
+  ).toMatchObject([
+    [1, 2],
+    [7, 8],
+    [16, 16],
+  ]);
+});
+
+test("test case 7", () => {
+  expect(
+    intervalIntersection(
+      [
+        [2, 5],
+        [6, 9],
+        [18, 19],
+        [22, 38],
+        [40, 59],
+        [86, 87],
+      ],
+      [
+        [17, 18],
+        [20, 24],
+        [26, 27],
+        [28, 34],
+        [42, 43],
+        [48, 62],
+        [73, 78],
+        [80, 83],
+        [90, 95],
+        [96, 100],
+      ]
+    )
+  ).toMatchObject([
+    [18, 18],
+    [22, 24],
+    [26, 27],
+    [28, 34],
+    [42, 43],
+    [48, 59],
+  ]);
 });
